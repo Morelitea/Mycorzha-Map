ce545c57799c16e09c1bbb9f667c32c14165dab7
jordandrako <jordan@morels.family>
2025-02-07 14:44:10 -0800

    Scroll to top

--- src/utils/handleScrollClick.ts
+++ src/utils/handleScrollClick.ts
@@ -1,9 +1,10 @@
+import { replace } from "react-router-dom";
+
 export const handleScrollClick = (
   ev: React.MouseEvent<HTMLAnchorElement>
 ): void => {
   const target = ev.currentTarget as HTMLAnchorElement;
   if (target?.hash) {
-    ev.preventDefault();
     const id = target.hash.substring(1);
     const element = document.getElementById(id);
     if (element) {
@@ -12,6 +13,11 @@ export const handleScrollClick = (
         behavior: "smooth",
       });
     }
+  } else {
+    window.scrollTo({
+      top: 0,
+      behavior: "instant",
+    });
   }
 };
 
