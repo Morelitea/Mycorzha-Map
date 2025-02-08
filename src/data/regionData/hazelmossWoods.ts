71ca54a125f48ff7b1ce92bc2226c526af8a7ebf
jordandrako <jordan@morels.family>
2025-02-08 15:21:44 -0800

    Hazelmoss Woods content

--- src/data/regionData/hazelmossWoods.ts
+++ src/data/regionData/hazelmossWoods.ts
@@ -2,22 +2,22 @@ import { RegionData } from "../../types/Regions";
 
 export const hazelmossWoods: RegionData = {
   majorContent:
-    "In Luna Valley, delicate pastel blossoms sway in the breeze, and every tea captures the spirit of tranquil meadows and sunlit springs. From floral motifs to soft hues of pink, lavender, and pale gold, Luna Valley’s wares reflect its peaceful charm and serene beauty. Made from fertile volcanic soil fed by streams from the surrounding mountain range Luna Valley has endless food and comfortable weather year round that makes it the most populated area in the Isles next to Cove City itself.",
+    "Nestled between the southern lakes and the upper delta of the Luna River this deciduous forest is filled with American chestnuts, oaks, and maples as far as the eye can see. The Hazelmoss Woods are a breathtaking celebration of nature’s golden hour, where the world is forever draped in the warm, inviting hues of autumn. This deciduous forest is a kaleidoscope of amber, russet, and ochre tones, with light woods that glow as though kissed by a perpetual sunset. The air is rich with the earthy aroma of fallen leaves, and sunlight filters through the canopy in soft, dappled patterns, casting a magical glow over the leafy forest floor.",
   subsections: [
     {
       name: "Known Residents",
       id: "known-residents",
       subsectionType: "creature",
-      content: "/data/lunaValley.json",
+      content: "",
     },
     {
       name: "Culture",
       id: "culture",
-      content: `Luna Valley rests in the crater like cliffs that are at the edge of the crescent mountains. The huge glacial peaks surrounding it do well at preventing bad weather, and so there is rarely a stormy night to worry about, with the sun shining 300+ days of the year overhead. The volcanic soil from the mountains surrounding it is filled with nutrients and food grows here freely and with great ease. Because of this most of the population of the Isles resides here or in Cove City nearby. So as not to disturb the top soil many who live here burrow underground to build permanent residences, finding the cool break from the sun refreshing, and the smell of the earth a comfort at the end of a long day. Much of the valley is covered in wild grasses and flowers, with a constant buzz of honey bees working away. Small pockets of fruit trees and bushes are interspersed throughout. There are many small creeks which stream throughout the area, all feeding into the main river network.
+      content: `The Hazelmoss Woods is in the southern part of the Isles,where the warm air and humidity of the marshes not far away keep the area relatively damp and always smelling vaguely like petrichor after a rainstorm. Rolling hills throughout the Woods keep most of the residences safely away from the flood zones of the Luna Valley River delta that streams its way through. As a deciduous forest there is always an underbed beneath the trees of old leaves, sprouted through with mushrooms and cultivated plant life. The hills provide ample drainage while the delta and humid air provide plenty of water allowing for some unique plant life to grow well here in both moist and dry soils. This is why it is the area of choice for the few in Mycorzha who dedicate their lives to plants and agriculture.
 
-Fable falls, where the water feeds the river from the glaciers, is a sight to behold. Huge and roaring, anything nearby is covered in a fine mist, the river below deep from centuries of the raging water pounding against the ground. Luna Valley is deceptively large, and traveling from one end to the other takes 4-5 days so those who live this far upstream place trade goods in tight water weaved baskets and send them down to Coral Cap Cove to be retrieved and traded for them in exchange for whatever they indicate on the conveniently naturally waterproof mushroom paper. A pully system is erected along the rivers, which brings the baskets back up on hooks filled with the traded goods. The vine ropes holding the baskets are pulled along with a simple water wheel (think like a ski lift system). Occasionally mischievous little ones try to ride in the baskets, a fun past time until you stay in one a little too long and get lost. Some of the creeks through the valley are large enough to carry smaller baskets, which those who live along the riverbed will transfer into the heavier duty baskets to send along for trade. A few creatures do this full time, hiking to deliver the traded goods back to those who live on the edges of the valley.
+As with all the Isles, Hazelmoss Woods is a cultivated food forest with various nut trees, berry bushes, medicinal herbs, and anything else you could imagine cultivated for the best harvests. However, some choose to reside here their whole lives working to grow things, using different systems or developing new plants. Often a family affair, these “farms” typically specialize and work together to grow unique crops, like the imported tea trees grown the by the Theacen family or the bursting unique colors of the specialty flowers cultivated by the Asteria family. Land is shared freely and so the nomadic clans often come through during large harvest times both for their own harvest and to lend a helping hand preserving these specialty crops for the families to use for trade.
 
-Locals here heavily value "doers" and so many here create primarily functional arts: blankets, baskets, pictography tiles, and so on. There is abundant natural clay, along with permanent settlements, and so brick and pottery is common. Due to the large river there are many river creatures in the area and a favorite food here is fish. The culture results in many desiring to find a problem and "solve it", with many tool inventors finding ways to work with the land and build creative new solutions. This can get out of hand with the young ones, who don't always value the traditional ways in which the elderly here tell them to temper their greed. Certainly after they invent the next amazing whosamawatsit they will all see that we are just making things *more efficient*.`,
+Not many live here year-round as the winters here, like for any deciduous forest, are dormant and lack easy access to food. Cottages are often too far apart to see each other across the way, but all the same somehow the neighbors seem to know all the latest gossip. Like most of the Isles, Hazelmoss Woods homes are often multi-generational, sheltering and teaching the whole family - an organized chaos often overseen by the family matriarch. Families pass down their knowledge and care of the land to the next generation, along with memories shared so that family may live on forever. Each family cottage in Hazelmoss cherishes this ancestral knowledge, and so each family often also maintains a family shrine where they remember loved one’s past and give a portion of each year’s harvests.`,
     },
     {
       name: "Regional Differences",
@@ -29,28 +29,29 @@ Locals here heavily value "doers" and so many here create primarily functional a
         {
           name: "Dialect",
           id: "dialect",
-          content:
-            "Not much of a strong accent due to the constant moving buzz that comes with being in a place like Luna Valley. A lot of words are borrowed from the Beyonders, making their way into local slang and confusing folks that aren't from around there.",
+          content: `The Hazelmoss dialect has a rhythmic, musical quality with a smooth but sometimes clipped pronunciation. It often drops the "g" in "-ing" words (runnin', fishin'), lengthens vowels (like "far" sounding more like "fahr"), and softens certain consonants (like "holler" instead of "hollow"). Notable features include the use of "ain’t," double modals like "might could."`,
         },
         {
           name: "Spirituality",
           id: "spirituality",
-          content: `Luna Valley is a less spiritual part of the land, due in part to the blended cultures, but also because it can be more disconnected from natures cycles in the ever plentiful valley. That said, one would never take a mushroom without telling the land thank you for its blessings, even though at this point its as normal as saying "bless you" after a sneeze. Like many cultures which rely on a key water source funerals are held by boat crafts sent out to sea. Creatures are wrapped in a clean mushroom cloth and adorned with their favorite teas, flowers, and other things from life. The River goddess is believed to carry you down Luna and into the mysts, a place "in between" which cannot be accessed by the living. The River creatures of Luna Valley have a unique religious belief you can learn more about under their subculture section.`,
+          content: `The creatures of Hazelmoss also worship the Mysts and the Isles like most, but believe both to be made from the souls of their ancestors. Each mushroom a sign that great grandma is watching and approves of your care for the soil, and each sunny day where the Mysts break through to grow the crops seen as a blessing from those who came before you. This is why all who live here give a portion of their yearly harvests as donations to their ancestral shrines, believing all good things came at least in part due to their kindness and oversight.`,
         },
         {
           name: "Art",
           id: "art",
-          content: `Art here is heavily influenced by the city, colorful and bright with distinct geometric patterns throughout, but the tones are softened into pastels to blend with the flower fields that cover much of the valley. Creatures that live here tend to value symmetry and practicality to their designs. Clutter is generally frowned upon and so grand ornate designs are seen as rather eccentric. As one of the few places with permanent residences, some going back many generations in one family, the homes found here tend to be very self expressive from the many years of careful cultivation. Simple braids made from old vines pulled from the river shores are used as accents, and river baskets that once carried trade goods but sprung a leak are reused as plant pots where many grow delightful herbs and flowers not from the valley for year round enjoyment.`,
+          content: `Art here is deeply personal, often created using natural materials like wood, clay, and fabric harvested straight from the soil of each families farming region. It tells stories about daily life, family, and the land, using bright colors, detailed patterns, and strong textures. People in the region make quilts with designs that pass down family history, carve figures and scenes from wood, and shape pottery from the river delta clay that reflects the colors of the autumn leaves. The art feels warm, lived-in, and connected to the past, celebrating both hard work and creativity.`,
         },
         {
           name: "Holidays",
           id: "holidays",
-          content: `Like all of the land Luna Valley celebrates the 4 major turns of the season. Holidays here are HUGE, with each day celebrating the turn of the season often having the burrows and nests adorned with natural seasonal decor, and community feasts held outside where people share their personal food and teas with others freely. Some of the more competitive individuals go all out and try to create new and artistic food presentations for these to wow their neighbors, even planning for just one holiday for a whole year to pull this off.Those who are religious or spiritual, and many who just culturally feel its a little whimsical, will make these foods from the blessed objects of that given holiday. Traditionally in Luna valley a blessing is baked (or steeped) into the dishes you prepare and thought to bring good (or bad...) luck to those who partake in your food. This is mostly taken as a silly thought, but its not abnormal to blame a bit of bad luck on your neighbors cooking in a playful jab. Those who are trying to impress the parents of a lover would always be sure to say that the parents food was to blame for all their recent success.`,
+          content: `While each of the 4 major holidays are celebrated the 2 holidays focused on natural life cycles (coming of age with Nimmerith and death with Faelivrin) are by far the most centered in the lives of the creatures here.
+
+These holidays also often mark the start and end of the growth cycle that year, and so families will often spend weeks preparing to celebrate. Decorations, feasting, and taking time to see your friends, family, and neighbors become the only activity of importance until the festivities are over.`,
         },
         {
           name: "Common Activities and Interests",
           id: "common-activities-and-interests",
-          content: `Luna valley is a bit more fast paced than most of the land, with people who really enjoy exploring, river tubing, and creating community events to celebrate holidays or simple play some games together that keeps people out and about. It is not uncommon here for people to create hobby groups that meet just to do a shared activity like painting, trivia nights, or creating tea blends together. The blend of cultures and creatures through the area means most people have friends of all types, and the shared hobbies help them to build the closer bonds that are more common for the less populated areas.`,
+          content: `As many stay fairly busy with their families and caring for their plants the most common interest here is being an introvert. Nothing beats a cozy night by the fireplace after a long days work, or a fresh rinse in the steam to cool down in the summer.`,
         },
       ],
     },
@@ -64,49 +65,87 @@ Locals here heavily value "doers" and so many here create primarily functional a
         {
           name: "Mushrooms",
           id: "mushrooms",
-          content:
-            "✦ Fairy Ring (Marasmius oreades) ✦ Field Mushroom (Agaricus campestris) ✦ Shaggy Ink Cap (Coprinus comatus) ✦ False Parasol (Chlorophyllum molybdites) ✦ White Dunce Cap (Conocybe apala) ✦ Weeping Widow (Lacrymaria velutina) ✦ Parasol Mushroom (Macrolepiota procera) ✦ Scruffy Twiglet Mushroom (Tubaria furfuracea) ✦ Common Brown Morel (Morchella esculenta) ✦ Destroying Angel (Amanita bisporigera) ✦ Puffball (Calvatia gigantea) ✦ Basket Stinkhorn (Lysurus cruciatus) ✦ Pinwheel (Marasmius rotula) ✦ White Dapperling (Leucoagaricus leucothites) ✦ Fly Agaric (Amanita muscaria) – Known for its iconic red-and-white cap.",
+          content: `✦ Hen of the Woods (Grifola frondosa)
+
+✦ Turkey Tail (Trametes versicolor)
+
+✦ Dead Man’s Fingers (Xylaria polymorpha)
+
+✦ Scarlet Elf Cup (Sarcoscypha coccinea)
+
+✦ Eastern Destroying Angel (Amanita bisporigera)
+
+✦ Cinnabar Chanterelle (Cantharellus cinnabarinus)
+
+✦ Shaggy Mane (Coprinus comatus)
+
+✦ Bearded Tooth (Hericium erinaceus)
+
+✦ Witches’ Butter (Tremella mesenterica)`,
         },
         {
           name: "Flowers",
           id: "flowers",
-          content:
-            "✦ Echinacea (Echinacea purpurea) – Purple coneflower, loved by pollinators. ✦ Gloriosa Daisy (Rudbeckia hirta) – Bright yellow petals with dark centers. ✦ False Indigo (Baptisia australis) – Tall blue flowers in early summer. ✦ Aromatic Aster (Symphyotrichum oblongifolium) – Pale lavender flowers, blooming in fall. ✦ False Sunflower (Heliopsis helianthoides) – Bright yellow flowers resembling true sunflowers. ✦ Butterfly Weed (Asclepias tuberosa) – Bright orange flowers, a favorite of monarch butterflies. ✦ Wild Bergamot (Monarda fistulosa) – Light purple flowers with a strong fragrance, attracting bees and butterflies.",
+          content: `✦ Goldenrod (Solidago spp.)
+
+✦ Trillium (Trillium spp.)
+
+✦ Aster (Aster spp.)
+
+✦ Lady’s Slipper Orchid (Cypripedium spp.)
+
+✦ Black-eyed Susan (Rudbeckia hirta)
+
+✦ Wild Geranium (Geranium maculatum)`,
         },
         {
-          name: "Fruits",
-          id: "fruits",
-          content:
-            "✦ Chokeberries (Aronia melanocarpa) – Produces small, dark, antioxidant-rich berries. ✦ Persimmons (Diospyros virginiana) – Small, sweet fruits that ripen in the fall. ✦ Wild Grapes (Vitis riparia) – Vines that produce small, tart grapes, often found climbing trees. ✦ Pawpaws (Asimina triloba) – Edible tropical-like fruit with custardy texture. ✦ Purple Passionfruit (Passiflora incarnata) – Vining plant with edible fruits and beautiful purple flowers.",
+          name: "Trees",
+          id: "trees",
+          content: `✦ Sugar Maple (Acer saccharum)
+
+✦ Red Oak (Quercus rubra)
+
+✦ American Beech (Fagus grandifolia)
+
+✦ White Birch (Betula papyrifera)
+
+✦ Black Cherry (Prunus serotina)
+
+✦ Eastern Hemlock (Tsuga canadensis)`,
         },
         {
           name: "Grasses and Shrubs",
           id: "grasses-and-shrubs",
-          content:
-            "✦ Wild Oat (Chasmanthium latifolium) – A tough, graceful grass with arching seed heads. ✦ Little Bluestem (Schizachyrium scoparium) – A tall, clumping grass often found in open woodlands. ✦ Switchgrass (Panicum virgatum) – Tolerates poor soils and provides excellent cover for wildlife. ✦ Indian Grass (Sorghastrum nutans) – Tall, golden grass found in dry, sunny woodland meadows. ✦ Eastern Redbud (Cercis canadensis) – A small, understory tree with bright pink flowers in the spring, often found along woodland edges. ✦ American Hazelnut (Corylus americana) – A shrub with edible nuts, growing in sunny, open spaces of woodlands. ✦ New Jersey Tea (Ceanothus americanus) – A low, deciduous shrub with white flowers, thriving in dry, rocky soils along the cliff edges of the valley. ✦ Witch Hazel (Hamamelis virginiana) – A shrub with yellow flowers in the fall, known for its medicinal properties. ✦ Ninebark (Physocarpus opulifolius) – A tough shrub with peeling bark, found in rocky or sandy soils, providing cover for wildlife.",
+          content: `✦ Indian Grass (Sorghastrum nutans)
+
+✦ Blue Stem Grass (Andropogon gerardii)
+
+✦ Bracken Fern (Pteridium aquilinum)
+
+✦ Ostrich Fern (Matteuccia struthiopteris)
+
+✦ Mountain Laurel (Kalmia latifolia)
+
+✦ Red-twig Dogwood (Cornus sericea)
+
+✦ Spicebush (Lindera benzoin)
+
+✦ Highbush Blueberry (Vaccinium corymbosum)
+
+✦ Wild Azalea (Rhododendron spp.)`,
         },
         {
           name: "Other Plants",
           id: "other-plants",
-          content:
-            "✦ Bur Oak (Quercus macrocarpa) – A drought-tolerant oak, often found in savannas and open woodlands. ✦ Shagbark Hickory (Carya ovata) – A tall, open-grown tree with peeling bark, thriving in well-drained soils. ✦ Black Walnut (Juglans nigra) – Often found along streams or in fertile soils, providing dappled shade. ✦ Hackberry (Celtis occidentalis) – Tolerant of poor soils and windy conditions, with small fruits that attract wildlife.",
-        },
-      ],
-    },
-    {
-      name: "Subcultures",
-      id: "subcultures",
-      content:
-        "Luna valley is large enough that some of it's subcultures are also considered regions.",
-      subsectionType: "subculture",
-      subsections: [
-        {
-          name: "Selene Cliffs",
-          id: "selene-cliffs",
-          link: "/region/selene-cliffs",
-          image: "selene-cliffs.png",
-          content:
-            "Surrounding cliffs often populated by the birds of the Isles.",
+          content: `✦ Moss (Various species)
+
+✦ Lichen (Various species)
+
+✦ Wild Grape Vine (Vitis spp.)
+
+✦ Creeping Juniper (Juniperus horizontalis)
+
+✦ Mayapple (Podophyllum peltatum)`,
         },
       ],
     },
