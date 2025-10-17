766e892a8a9ef5cb6840fdfeff298c418e650fcf
Jordan Janzen <jordan@morels.me>
2025-10-17 09:23:02 -0700

    Update sun dried sands

--- src/data/regionData/sunDriedSands.ts
+++ src/data/regionData/sunDriedSands.ts
@@ -10,11 +10,15 @@ export const sunDriedSands: RegionData = {
       subsectionType: "creature",
       content: "",
     },
-    // {
-    //   name: "Culture",
-    //   id: "culture",
-    //   content: ``,
-    // },
+    {
+      name: "Culture",
+      id: "culture",
+      content: `Life in the desert revolves around the sky, the day and night, the wind and weather, and the stars. Creatures of the Sun Dried Sands avoid the harsh heat of the day, and many here are nocturnal or do most of their work in the mornings or evenings. This leaves the the heat of the day for resting, or quiet socializing in burrows and dwellings carved from desert rock or made from adobe. This is a time for talking, teaching, and crafting. Taking from the beauty of the painted desert rock and the colors of sky and stream, the folk of the Sun Dried Sands decorate everything from their homes to pottery to baskets and blankets with colorful geometric patterns, evoking the reds and oranges of sunsets, the dark blue-black of the night sky, and the turquoise of water. The folk of the Sands are known for their pottery and their weaving, spinning various fibers into textiles of incredible complexity and beauty.
+
+The Sands are a place of extremes, and the creatures who live there have taken the lessons of the desert to heart. Traditions play a large part in lives dictated by sun and storm, and the rules of life are strict, enforced by the Isles themselves. Sands-folk are self-reliant and make do with what’s on hand, but all understand the inter-connected nature of life in the canyons; take too much from the well, and those downstream will perish. Roles within the local tribes and clans are clearly-defined, and responsibility to the community comes first.
+
+Wind and weather, sky and stars are companions of those who live in the Sun Dried Sands as much as any creature. The desert breezes howl and whistle through the canyonlands, and the creatures who live there have discovered how to harness the wind using sails woven of yucca fiber. Wind power has been used for many seasons in the Sands, but recently the Sands-folk have set their sights further still, crafting sleds and vehicles that glide over the sands, and even soar above the towering rock spires. Kites are popular both for sport and communication, and holiday celebrations find the sky a riot of swirling, fluttering color. And as day sets into night, the desert sky becomes a wash of stars that is breathtaking to behold. The creatures who live here, far from merely gazing skyward, use the stars to navigate by, and for inspiration in tales and artwork, in ceremony and ritual. The stars and constellations play a pivotal role in desert spirituality, and many look to the night sky to plot their futures and what lies in store for them.`,
+    },
     {
       name: "Regional Differences",
       id: "regional-differences",
@@ -25,12 +29,14 @@ export const sunDriedSands: RegionData = {
         {
           name: "Dialect",
           id: "dialect",
-          content: `The Sun Dried Sands dialect is fast-paced and expressive, with words tumbling out in quick bursts like pebbles down a canyon wall. Sentences are lively and rhythmic, often ending in soft whistles or tongue-clicks (a habit passed down from desert guides who once used subtle sounds to communicate across hot, wide distances without disturbing the landscape), with rolled R’s, dropped endings, and emphatic hand gestures.`,
+          content: `The Sun Dried Sands dialect is fast-paced and expressive, with words tumbling out in quick bursts like pebbles down a canyon wall. Sentences are lively and rhythmic, often ending in soft whistles or tongue-clicks (a habit passed down from desert guides who once used these sounds to communicate across canyons), with rolled R’s, dropped endings, and emphatic hand gestures. Sign language is prominent in the Sands, as a way to communicate across the vast, hot expanses.`,
         },
         {
           name: "Spirituality",
           id: "spirituality",
-          content: `In Sun Dried Sands, thunderbirds are revered as powerful guardians who soar between the realms of the Mysts and Isles, carrying messages and maintaining balance. Thunderstorms are sacred events, moments when the veil thins and the energy of life and death surges through the desert, reminding all that the land itself is alive and watching. Many critters believe these forces guide their path, shaping the rhythm of the seasons and the pulse of the sands beneath their feet.`,
+          content: `In Sun Dried Sands, thunderbirds are revered as powerful guardians who soar between the realms of the Mysts and Isles, carrying messages and maintaining balance. Thunderstorms are sacred events, moments when the veil thins and the energy of life and death surges through the desert, reminding all that the land itself is alive and watching. Many critters believe these forces guide their path, shaping the rhythm of the seasons and the pulse of the sands beneath their feet.
+
+There are many also who trace the arc and curve of stars across the heavens, and look to the night skies over the desert for guidance in body and spirit both. Astronomy and astrology are popular in the Sands, and the stars are said to be able to weave fate and fortune.`,
         },
         {
           name: "Art",
@@ -45,7 +51,11 @@ export const sunDriedSands: RegionData = {
         {
           name: "Common Activities and Interests",
           id: "common-activities-and-interests",
-          content: `Pottery is unique to this region! Beautiful red clay in the soil and the hot sun makes for some of Mycorzha's best ceramics. Everyone loves playing around in the sand and mud.`,
+          content: `Pottery is unique to this region! Beautiful red clay in the soil and the hot sun makes for some of Mycorzha's best ceramics. Everyone loves playing around in the sand and mud, and the painted cliffs inspire striking designs and uses of clay and sand.
+
+The desert folk are also known for their weaving, and Sands-made textiles are strong and durable, spun from yucca and other plant fibers (and from the fleece of the natives in the spring as they prepare for the heat of summer). The resulting yarns and thread are dyed in a bright variety of hues, and woven into rugs, blankets, and sails of great complexity. Weaving techniques are passed down matrilineal lines and are a hallmark of the craft.
+
+The omnipresent winds of the desert have created new sports and activities among the creatures of the Sands as newer and lighter weight textiles have been developed. Sand-skimming in sail-driven craft, gliding, and kite flying are all popular pastimes, and competition ranges from leisurely to fierce.`,
         },
       ],
     },
